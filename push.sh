bd742627398a3695ac40170e475708286ff85c0a
blcrlsn2 <blcrlsn2@illinois.edu>
2017-11-10 14:40:10 -0500

    possible fix

--- push.sh
+++ push.sh
@@ -6,13 +6,13 @@ setup_git() {
 }
 
 commit_website_files() {
+  git checkout master
   git add . *.md
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git checkout master
-  git push "https://${GITHUB_TOKEN}@github.com/blcarlson01/github-trending.git" origin HEAD:master > /dev/null 2>&1
+  git push "https://${GITHUB_TOKEN}@github.com/blcarlson01/github-trending.git" origin master > /dev/null 2>&1
 }
 
 setup_git
