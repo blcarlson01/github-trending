2da1d429e4fbdb5d4e163cc522294b6d96fbbe89
blcrlsn2 <blcrlsn2@illinois.edu>
2017-11-10 17:21:14 -0500

    fix

--- push.sh
+++ push.sh
@@ -13,7 +13,7 @@ commit_website_files() {
 
 upload_files() {
   git remote set-url origin https://blcarlson01@github.com/blcarlson01/github-trending.git > /dev/null 2>&1
-  git push --force --quiet "https://blcarlson01:${github_token}@github.com/blcarlson01/github-trending.git" origin master > /dev/null 2>&1
+  git push --force --quiet "https://${github_token}@github.com/blcarlson01/github-trending.git" origin master > /dev/null 2>&1
 }
 
 setup_git
