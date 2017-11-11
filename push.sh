5d73d6ee77e0d4a8ec55c96baf03ef3199dd44b2
blcrlsn2 <blcrlsn2@illinois.edu>
2017-11-10 20:53:07 -0500

    missing json file

--- push.sh
+++ push.sh
@@ -2,3 +2,7 @@
 
 mkdir results
 cp *.md results/
+cp *.json results/
+git clone -b gh-pages https://github.com/blcarlson01/github-trending.git
+cp github-trending/*.md results/
+cp github-trending/*.json results/
