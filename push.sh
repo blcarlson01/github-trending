4f678ea4f0f48ad8a1ad038f397d20b1cf5e75b5
blcrlsn2 <blcrlsn2@illinois.edu>
2017-11-10 19:18:49 -0500

    pages

--- push.sh
+++ push.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
-
-commit_website_files() {
-  git checkout master
-  git add . *.md
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
-
-setup_git
-commit_website_files
+mkdir results
+cp *.md results/
