76985ff5ccb2dd3a714c891541b7dfcdddc450e0
blcrlsn2 <blcrlsn2@illinois.edu>
2017-11-10 20:30:33 -0500

    added json feature, removed git push code

--- scraper.py
+++ scraper.py
@@ -6,30 +6,24 @@ import requests
 import os
 import time
 from pyquery import PyQuery as pq
-'''
-def git_add_commit_push(date, filename):    
-    cmd_git_add = 'git add {filename}'.format(filename=filename)
-    cmd_git_commit = 'git commit -m "{date}"'.format(date=date)
-    cmd_git_push = 'git push -u origin master'
+import json
 
-    #os.system(cmd_git_ssh)
-    #git(cmd_git_add)
-    #git(cmd_git_commit)
-    #git(cmd_git_push)
-    git_ssh_identity_file = os.path.expanduser('~/.ssh/id_rsa')
-    git_ssh_cmd = 'ssh -i %s' % git_ssh_identity_file
-    with Git().custom_environment(GIT_SSH_COMMAND=git_ssh_cmd):
-        repo = git.Repo(search_parent_directories=True)
-        repo.git.add(filename)    
-        repo.git.push("origin", "HEAD:refs/for/master") 
-        repo = Repo(self.rorepo.working_tree_dir)
-'''
 def createMarkdown(date, filename):
     with open(filename, 'w') as f:
         f.write("## " + date + "\n")
 
+def create_json(data, title, url, description):
+    data['site'].append({  
+    'title': title,
+    'url': url,
+    'description': description
+    })
 
-def scrape(language, filename):
+def write_json(data, filename):
+    with open(filename, 'w') as outfile:  
+        json.dump(data, outfile)    
+
+def scrape(language, filename, json_filename):
 
     HEADERS = {
         'User-Agent'		: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0',
@@ -46,12 +40,14 @@ def scrape(language, filename):
 
     d = pq(r.content)
     items = d('ol.repo-list li')
+    json_data = {}
+    json_data['site'] = [] 
 
     # codecs to solve the problem utf-8 codec like chinese
     with codecs.open(filename, "a", "utf-8") as f:
         f.write('\n#### {language}\n'.format(language=language))
 
-        for item in items:
+        for item in items:            
             i = pq(item)
             title = i("h3 a").text()
             owner = i("span.prefix").text()
@@ -60,23 +56,22 @@ def scrape(language, filename):
             url = "https://github.com" + url
             # ownerImg = i("p.repo-list-meta a img").attr("src")
             # print(ownerImg)
+            create_json(json_data, title, url, description)
             f.write(u"* [{title}]({url}):{description}\n".format(title=title, url=url, description=description))
 
-
+    write_json(json_data, json_filename)
+    
 def job():
 
     strdate = datetime.datetime.now().strftime('%Y-%m-%d')
     filename = '{date}.md'.format(date=strdate)
+    json_filename = '{date}.json'.format(date=strdate)
 
     # create markdown file
     createMarkdown(strdate, filename)
 
     # write markdown
-    scrape('java', filename)
-
-    # git add commit push
-    #git_add_commit_push(strdate, filename)
-
+    scrape('java', filename, json_filename)
 
 if __name__ == '__main__':
         job()
